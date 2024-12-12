08579778f69c4f198e2d958041a6778b8f9a6d8a
Egor <Shambasovegor@yandex.ru>
2024-12-12 15:53:03 +0400

    Отрефакторил БЕЗ index.ts пока что

--- src/DesktopComponents/ExploreMarketplaceComponent/ExploreMarketplaceFolder/CardCase/CardCase.tsx
+++ src/DesktopComponents/ExploreMarketplaceComponent/ExploreMarketplaceFolder/CardCase/CardCase.tsx
@@ -3,12 +3,8 @@ import Card from "../Card/Card.tsx";
 import { CardSlice } from "@/ReduxFeatures/CardSlice/CardSlice.ts";
 import { connect, ConnectedProps } from "react-redux";
 import { fetchCardsAsync } from "@/Api/AsyncThunk/FetchCardsAsync.ts";
-/*import {useState} from "react";*/
-/*import Pagination from "../Paginator/Paginator.tsx";*/
 import { RootState } from "@/ReduxFeatures/Store/Store.ts";
 
-//Закомментировал Пагинатор
-//В {CardArray.map((cardItem: CardSlice) => ( заменить CardArray на currentItems чтобы работало
 const connector = connect(
   (state: RootState) => ({
     CardArray: state.CardSlice.cards,
@@ -20,24 +16,6 @@ const connector = connect(
 type CardCaseProps = ConnectedProps<typeof connector>;
 const CardCase = connector(
   ({ CardArray, isPending, isError }: CardCaseProps) => {
-    /*const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(8);
-    const totalPages = Math.ceil(CardArray.length / itemsPerPage);
-    const indexOfLastItem = itemsPerPage * currentPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = CardArray.slice(indexOfFirstItem, indexOfLastItem);*/
-
-    /* const handleNextPageClick = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage((prevPage) => prevPage + 1);
-        }
-    };
-    const handlePrevPageClick = () => {
-        if (currentPage > 1) {
-            setCurrentPage((prevPage) => prevPage - 1);
-        }
-    };*/
-
     if (isPending) {
       return <div>...loading...</div>;
     }
@@ -53,14 +31,6 @@ const CardCase = connector(
         {CardArray.map((cardItem: CardSlice) => (
           <Card key={cardItem.id} card={cardItem} />
         ))}
-        {/*<Pagination
-            onNextPageClick={handleNextPageClick}
-            onPrevPageClick={handlePrevPageClick}
-            disable={{
-                left: currentPage === 1,
-                right: currentPage === totalPages,
-            }}
-        />*/}
       </div>
     );
   }
