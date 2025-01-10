6d2c902c7a31caa852bb2aecab413ee49706b58e
Egor <Shambasovegor@yandex.ru>
2025-01-10 14:44:58 +0400

    убрал тестовую директорию

--- src/AppRoutes/AppRoutes.tsx
+++ src/AppRoutes/AppRoutes.tsx
@@ -5,7 +5,6 @@ import PageDiscover from "@/Pages/DiscoverPage/PageDiscover.tsx";
 import PageCreators from "@/Pages/CreatorsPage/PageCreators.tsx";
 import PageSells from "@/Pages/SellsPage/PageSells.tsx";
 import PageStats from "@/Pages/StatsPage/PageStats.tsx";
-import TEST_PAGE_FOR_FUTURE_COMPONENTS from "@/Pages/TEST_PAGE_FOR_FUTURE_COMPONENTS/TEST_PAGE_FOR_FUTURE_COMPONENTS.tsx";
 import PageCardExtended from "@/Pages/CardExtendedPage/PageCardExtended.tsx";
 
 const AppRoutes = () => {
@@ -18,7 +17,6 @@ const AppRoutes = () => {
           <Route path="/creators" element={<PageCreators />} />
           <Route path="/sells" element={<PageSells />} />
           <Route path="/stats" element={<PageStats />} />
-          <Route path="/test" element={<TEST_PAGE_FOR_FUTURE_COMPONENTS />} />
           <Route path="/card/:id" element={<PageCardExtended />} />
         </Routes>
       </div>
